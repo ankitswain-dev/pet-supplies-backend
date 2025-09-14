f502512ec6c0f3a8a9e2fd62497704963b3c35bd
ankitswain-dev <coderankit51@gmail.com>
2025-09-14 20:07:16 +0530

    Fix sequelize config for Postgres on Render

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -7,7 +7,9 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",   // 👈 VERY IMPORTANT
+    logging: false,
   }
 );
 
